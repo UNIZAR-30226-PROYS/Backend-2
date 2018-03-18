c51c5c93516f2b9804b29e30aaf8874daff19799
lAngelP <info@langelp.net>
2018-03-18 23:35:02 +0100

    Add user related tables

--- Database/create.sql
+++ Database/create.sql
@@ -38,3 +38,42 @@ CREATE TABLE public.followers
     user2 INT,
     CONSTRAINT followers_user1_user2_pk PRIMARY KEY (user1, user2)
 );
+
+CREATE TABLE public.song_reproductions
+(
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    song_id INT NOT NULL,
+    time BIGINT NOT NULL,
+    CONSTRAINT song_reproductions_users_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT song_reproductions_songs_id_fk FOREIGN KEY (song_id) REFERENCES songs (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE public.song_likes
+(
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    song_id INT NOT NULL,
+    time BIGINT NOT NULL,
+    CONSTRAINT song_reproductions_users_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT song_reproductions_songs_id_fk FOREIGN KEY (song_id) REFERENCES songs (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TYPE SOCIAL_NETWORK AS ENUM ('TW', 'IG', 'FB');
+
+CREATE TABLE public.user_social_networks
+(
+    user_id INT NOT NULL,
+    network SOCIAL_NETWORK NOT NULL,
+    value VARCHAR(128),
+    CONSTRAINT user_social_networks_user_id_network_pk PRIMARY KEY (user_id, network),
+    CONSTRAINT user_social_networks_users_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE public.user_values
+(
+    user_id INT PRIMARY KEY,
+    admin BOOLEAN DEFAULT FALSE NOT NULL,
+    CONSTRAINT user_values_users_id_fk FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+COMMENT ON TABLE public.user_values IS 'Mark which users are admins';
