76fd2f878a7fbb0c53891171f63f3f20c2d472c3
off94 <33528745+off94@users.noreply.github.com>
2018-05-18 17:34:13 +0200

    add/remove song album

--- RestAPI/src/main/java/es/eina/requests/AlbumRequests.java
+++ RestAPI/src/main/java/es/eina/requests/AlbumRequests.java
@@ -52,7 +52,7 @@ public class AlbumRequests {
 							if(year > 1900) {
 								EntityAlbum album = AlbumUtils.createAlbum(user, title, year, image);
 								if(album != null && AlbumCache.saveEntity(album)){
-									albumJSON.put("id", album.getAlbumId());
+									albumJSON.put("id", album.getId());
 									albumJSON.put("user_id", album.getUserId());
 									albumJSON.put("title", album.getTitle());
 									albumJSON.put("publishYear", album.getPublishYear());
@@ -126,6 +126,106 @@ public class AlbumRequests {
 		return obj.toString();
 
     }
+
+	/**
+	 * Add song to album.
+	 *
+	 * @param nick : ID from user author of album.
+	 * @param userToken : User's token.
+	 * @param albumId : ID from the album.
+	 * @param songId : ID from the song to add
+	 *
+	 * @return The result of this request.
+	 */
+	@Path("/{nick}/{albumID}/add")
+	@POST
+	public String addSongToAlbum(@PathParam("nick") String nick, @DefaultValue("") @FormParam("token") String userToken,
+						 @PathParam("albumID") long albumId, @FormParam("songId") long songId){
+		JSONObject obj = new JSONObject();
+
+		if(StringUtils.isValid(nick) && StringUtils.isValid(userToken)){
+			EntityUser user = UserCache.getUser(nick);
+			if(user != null){
+				if (user.getToken() != null && user.getToken().isValid(userToken)) {
+					EntityAlbum album = AlbumCache.getAlbum(albumId);
+					if (album != null) {
+						EntitySong song = SongCache.getSong(songId);
+						if(song != null){
+							if(album.addSong(song)) {
+								AlbumCache.updateAlbum(album);
+								obj.put("error", "ok");
+							}else{
+								obj.put("error", "alreadyAdded");
+							}
+						}else {
+							obj.put("error", "unknownSong");
+						}
+					}else{
+						obj.put("error", "unknownAlbum");
+					}
+				} else {
+					obj.put("error", "invalidToken");
+				}
+			}else{
+				obj.put("error", "unknownUser");
+			}
+		}else{
+			obj.put("error", "invalidArgs");
+		}
+
+		return obj.toString();
+
+	}
+
+	/**
+	 * Delete song from album.
+	 *
+	 * @param nick : ID from user author of album.
+	 * @param userToken : User's token.
+	 * @param albumId : ID from the album.
+	 * @param songId : ID from the song to add
+	 *
+	 * @return The result of this request.
+	 */
+	@Path("/{nick}/{albumID}/{songId}/delete")
+	@DELETE
+	public String removeSongFromAlbum(@PathParam("nick") String nick, @DefaultValue("") @FormParam("token") String userToken,
+						 @PathParam("albumID") long albumId, @PathParam("songId") long songId){
+		JSONObject obj = new JSONObject();
+
+		if(StringUtils.isValid(nick) && StringUtils.isValid(userToken)){
+			EntityUser user = UserCache.getUser(nick);
+			if(user != null){
+				if (user.getToken() != null && user.getToken().isValid(userToken)) {
+					EntityAlbum album = AlbumCache.getAlbum(albumId);
+					if (album != null) {
+						EntitySong song = SongCache.getSong(songId);
+						if(song != null){
+							if(album.removeSong(song)) {
+								AlbumCache.updateAlbum(album);
+								obj.put("error", "ok");
+							}else{
+								obj.put("error", "alreadyRemoved");
+							}
+						}else {
+							obj.put("error", "unknownSong");
+						}
+					}else{
+						obj.put("error", "unknownAlbum");
+					}
+				} else {
+					obj.put("error", "invalidToken");
+				}
+			}else{
+				obj.put("error", "unknownUser");
+			}
+		}else{
+			obj.put("error", "invalidArgs");
+		}
+
+		return obj.toString();
+
+	}
 
     static {
     	defaultAlbumJSON = new JSONObject();
