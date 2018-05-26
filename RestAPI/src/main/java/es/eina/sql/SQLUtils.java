baf14893173755206bf820469d30da3c8125d3b6
lAngel <info@langelp.net>
2018-05-26 09:39:47 +0200

    Update SQLUtils

--- RestAPI/src/main/java/es/eina/sql/SQLUtils.java
+++ RestAPI/src/main/java/es/eina/sql/SQLUtils.java
@@ -2,14 +2,19 @@ package es.eina.sql;
 
 import es.eina.sql.utils.HibernateUtils;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class SQLUtils {
 
     public static long getRowCount(String table, String where){
         long rowCount;
-        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+        Session session = HibernateUtils.getSession();
+        Transaction t = session.beginTransaction();
+        try{
             rowCount = (Long) session.createQuery("SELECT COUNT(e) as c FROM " + table + " e WHERE " + where).iterate().next();
+            t.commit();
         } catch (Exception e) {
+            t.rollback();
             e.printStackTrace();
             rowCount = -1;
         }
@@ -19,9 +24,13 @@ public class SQLUtils {
 
     public static long getRowCountSQL(String table, String where){
         long rowCount;
-        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+        Session session = HibernateUtils.getSession();
+        Transaction t = session.beginTransaction();
+        try{
             rowCount = session.createSQLQuery("SELECT COUNT(e) as c FROM " + table + " e WHERE " + where).list().size();
+            t.commit();
         } catch (Exception e) {
+            t.rollback();
             e.printStackTrace();
             rowCount = -1;
         }
