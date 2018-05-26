b878ce2daca704ab9b655d7828434ef990e8195e
lAngel <info@langelp.net>
2018-05-26 21:15:38 +0200

    Add Persistance Test

---
+++ RestAPI/src/test/java/es/eina/sql/entities/PersistenceTest.java
@@ -0,0 +1,61 @@
+package es.eina.sql.entities;
+
+import es.eina.TestBase;
+import es.eina.cache.UserCache;
+import es.eina.requests.AlbumRequests;
+import es.eina.sql.SQLUtils;
+import es.eina.sql.utils.HibernateUtils;
+import es.eina.utils.AlbumUtils;
+import es.eina.utils.SongUtils;
+import es.eina.utils.UserUtils;
+import org.json.JSONObject;
+import org.junit.*;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Date;
+
+public class PersistenceTest extends TestBase {
+
+    private EntityUser user;
+    private EntityAlbum album;
+    private EntitySong song;
+
+    @BeforeClass
+    public static void start() {
+        openDB();
+    }
+
+    @AfterClass
+    public static void stop() {
+        closeDB();
+    }
+
+    @Before
+    public void setupTest() {
+        //user = UserUtils.addUser("test-user", "a@a.net", "123456", "Username :D", "Random BIO", new Date(0), "ES");
+    }
+
+    @After
+    public void endTest() {
+
+    }
+
+    @Test
+    public void testCreateDeleteUser() {
+        System.out.println(">>>>>>>>>>>><<<<<<<<<<<<<<<<");
+        LoggerFactory.getLogger(PersistenceTest.class).debug("Init Persistance Test");
+        System.out.println(">>>>>>>>>>>><<<<<<<<<<<<<<<<");
+        user = UserUtils.addUser("test-user", "a@a.net", "123456", "Username :D", "Random BIO", new Date(0), "ES");
+
+        Assert.assertNotNull(user);
+        Assert.assertEquals(1, SQLUtils.getRowCountSQL("users", "nick = '" + user.getNick() + "'"));
+        Assert.assertEquals(1, SQLUtils.getRowCountSQL("sessions", "user_id = '" + user.getId() + "'"));
+
+        String nick = user.getNick();
+        long id = user.getId();
+        UserCache.deleteUser(user);
+        Assert.assertEquals(0, SQLUtils.getRowCountSQL("users", "nick = '" + nick + "'"));
+        Assert.assertEquals(0, SQLUtils.getRowCountSQL("sessions", "user_id = '" + id + "'"));
+    }
+
+}
