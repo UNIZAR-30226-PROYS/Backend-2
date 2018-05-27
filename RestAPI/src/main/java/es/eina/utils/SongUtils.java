eaabbd094afa85a5bd07685df754789e210bfcc2
lAngel <info@langelp.net>
2018-05-27 18:00:12 +0200

    Get Last listened songs

--- RestAPI/src/main/java/es/eina/utils/SongUtils.java
+++ RestAPI/src/main/java/es/eina/utils/SongUtils.java
@@ -5,9 +5,15 @@ import es.eina.crypt.Crypter;
 import es.eina.sql.entities.EntityAlbum;
 import es.eina.sql.entities.EntitySong;
 import es.eina.sql.entities.EntityUser;
+import es.eina.sql.utils.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
+import org.json.JSONArray;
 
 import javax.annotation.Nullable;
 import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
 
 public class SongUtils {
 
@@ -24,9 +30,28 @@ public class SongUtils {
     EntitySong addSong(EntityAlbum album, String title, String country) {
 
         EntitySong entitySong = new EntitySong(album, title, country);
-        album.addSong(entitySong);
+        if(album != null) {
+            album.addSong(entitySong);
+        }
 
         return SongCache.addSong(entitySong) ? entitySong : null;
+
+    }
+
+
+    public static JSONArray getLastListenedSongs(@NotNull EntityUser user, int limit){
+        limit = Math.max(1, limit);
+        JSONArray songs = new JSONArray();
+        try(Session s = HibernateUtils.getSession()){
+            NativeQuery q = s
+                    .createSQLQuery("SELECT song_id FROM user_listened_songs WHERE user_id = :id ORDER BY time DESC LIMIT "+limit+";")
+                    .setParameter("id", user.getId());
+            for(Object o : q.list()){
+                songs.put(o);
+            }
+        }
+
+        return songs;
 
     }
 }
